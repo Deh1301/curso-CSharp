b5a5fabd3aced8349c4f4bf4cc6a1fc4e6ef68a0
Andre Calixto <calixtoandre77@gmail.com>
2024-11-25 12:38:14 -0300

    aula construtores

--- classesMetodos/modulos/constructor/construtor.cs
+++ classesMetodos/modulos/constructor/construtor.cs
@@ -1,1 +1,50 @@
-﻿
+﻿namespace ClassesMetodos
+{
+    public class Carro
+    {
+        public string Modelo;
+        public int Ano;
+
+        public Carro(string modelo, int ano) //sem o this e so colocar m e a minusculo nos atributos do construtor
+        {
+            Modelo = modelo;
+            Ano = ano;
+        }
+
+        public Carro()
+        {
+            //Construtor padrão quando nao recebe nenhum parametro
+        }
+    }
+
+
+    class Construtores
+    {
+        public static void ExercicioCarro()
+        {
+            var carro2 = new Carro();    //construtor padrao
+            carro2.Modelo = "Corolla";
+            carro2.Ano = 2020;
+            Console.WriteLine($"O carro é {carro2.Modelo} e o ano é {carro2.Ano}");
+
+
+            Carro carro = new Carro("Honda Civic", 2007);
+            // carro.Modelo = "Honda Civic";
+            // carro.Ano = 2007;
+            Console.WriteLine($"O carro é {carro.Modelo} e o ano é {carro.Ano}");
+
+            var carro3 = new Carro()
+            {
+                Ano = 2010,
+                Modelo = "Celta"
+            };
+            Console.WriteLine($"O carro é {carro3.Modelo} e o ano é {carro3.Ano}");
+
+
+
+
+
+
+        }
+    }
+}
--- obj/Debug/net8.0/curso-CSharp.AssemblyInfo.cs
+++ obj/Debug/net8.0/curso-CSharp.AssemblyInfo.cs
@@ -10,12 +10,12 @@
 using System;
 using System.Reflection;
 
-[assembly: System.Reflection.AssemblyCompanyAttribute("classes e metodos")]
+[assembly: System.Reflection.AssemblyCompanyAttribute("curso-CSharp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a2d1a3e0bdfa22e4c1ecaa7ef1a66f30375752f1")]
-[assembly: System.Reflection.AssemblyProductAttribute("classes e metodos")]
-[assembly: System.Reflection.AssemblyTitleAttribute("classes e metodos")]
+[assembly: System.Reflection.AssemblyProductAttribute("curso-CSharp")]
+[assembly: System.Reflection.AssemblyTitleAttribute("curso-CSharp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
 
 // Gerado pela classe WriteCodeFragment do MSBuild.
