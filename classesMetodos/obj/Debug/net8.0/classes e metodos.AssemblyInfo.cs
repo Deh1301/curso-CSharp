9f062085cef822fc9570901d107b1a009334d14e
ANDRE CALIXTO DOS SANTOS <129115005+Deh1301@users.noreply.github.com>
2024-11-27 19:40:37 -0300

    metodo retorno

--- classesMetodos/obj/Debug/net8.0/classes e metodos.AssemblyInfo.cs
+++ classesMetodos/obj/Debug/net8.0/classes e metodos.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("classes e metodos")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a2d1a3e0bdfa22e4c1ecaa7ef1a66f30375752f1")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7449e1a5da94f84fbfa4b4b5c4b31cdc4c713619")]
 [assembly: System.Reflection.AssemblyProductAttribute("classes e metodos")]
 [assembly: System.Reflection.AssemblyTitleAttribute("classes e metodos")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
