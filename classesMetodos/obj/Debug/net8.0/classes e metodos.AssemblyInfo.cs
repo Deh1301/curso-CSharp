3f144b5447a0d6e0ffdb01f104a65d3492ffeb57
Andre Calixto <calixtoandre77@gmail.com>
2025-01-03 15:10:57 -0300

    Enviando

--- classesMetodos/obj/Debug/net8.0/classes e metodos.AssemblyInfo.cs
+++ classesMetodos/obj/Debug/net8.0/classes e metodos.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("classes e metodos")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fe710b13d778387e0f54949c820afd62a8854568")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4050334c4aee1d0665250f794750e04ac6c80f65")]
 [assembly: System.Reflection.AssemblyProductAttribute("classes e metodos")]
 [assembly: System.Reflection.AssemblyTitleAttribute("classes e metodos")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
