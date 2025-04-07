2399ae8788eb2b33e80b48a745b54d4a196d17f8
Deh1301 <calixtoandre77@gmail.com>
2025-04-07 20:05:14 -0300

    adicionando ao modulo de POO

--- orientacaoObjeto/Program.cs
+++ orientacaoObjeto/Program.cs
@@ -1,2 +1,21 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using System.Collections;
+
+namespace orientacaoObjeto{
+    class OrientarObjeto{
+        public static void Main(string[] args){
+            System.Console.WriteLine("\nBem Vindo ao modulo de orientação a objeto");
+            
+            System.Console.WriteLine("\n1-Abstração");
+
+            System.Console.WriteLine("\nEscolha o exercicio:");
+
+            string escolha = Console.ReadLine();
+            switch(escolha){
+                case "1":
+                Abstract.Executar();
+                break;
+        }
+    }
+  }
+}
---
+++ orientacaoObjeto/modulos/abstracao/abstract.cs
@@ -0,0 +1,8 @@
+using System;
+namespace orientacaoObjeto{
+    class Abstract{
+        public static void Executar(){
+            System.Console.WriteLine("ola");
+        }
+    }
+}
